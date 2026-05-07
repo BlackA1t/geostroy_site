b3d6b16ace578c83f3379ffe60314c7f7110b3bb
blackalt <vadim.groshkov03@gmail.com>
2026-05-07 19:56:51 +0300

    Scroll to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,3 +91,21 @@
 
         // Run counter animation when hero is visible
         setTimeout(animateCounters, 500);
+
+        // Scroll to top button
+        const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 500) {
+                scrollTopBtn.classList.add('visible');
+            } else {
+                scrollTopBtn.classList.remove('visible');
+            }
+        });
+
+        scrollTopBtn.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        });
